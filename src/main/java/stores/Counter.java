f731972f872284f32620a7e0372130e59847378c
RichardXum <2038113310@qq.com>
2024-04-06 21:57:13 +0800

    CS0445A2

---
+++ src/main/java/stores/Counter.java
@@ -0,0 +1,69 @@
+package stores;
+
+import interfaces.ICounter;
+
+/**
+* A generic implementation of the ICounter iterface.
+*/
+public class Counter<E> implements ICounter<E> {
+
+    MyArrayList<Count<E>> counts = new MyArrayList<>();
+    
+    // INCOMPLETE.
+    public int getCount(E element) {
+        int index = 0;
+        for(int i = 0; i < counts.size();i++){
+            if(counts.get(i).equals(element)){
+                index++;
+            }
+        }// Returns the count of an item (the number of times it was added to the list).
+        return index;
+    }   
+
+    // INCOMPLETE.
+    public boolean add(E element) {
+        Count<E> count = find(element);
+        if (count == null) {
+            counts.add(count);// If the element does not exist, add it as a new count object to counts.
+        } else {
+            // If the element does exist, increment its count value.
+        }
+        return true;
+    }
+
+    // INCOMPLETE.
+    public int size() {
+        // Returns the total count of all items in the list.
+        return counts.size();
+    }
+
+    // Returns the count object related to element.
+    // Returns null if the element does not exist.
+    private Count<E> find(E element) {
+        for (int i=0;i<counts.size();i++) {
+            if (counts.get(i).getObj().equals(element)) {
+                return counts.get(i);
+            }
+        }
+        return null;
+    }
+
+    public void clear() {
+        counts.clear();
+    }
+
+    public boolean contains(E element) {
+        return find(element) != null;
+    }
+
+    public boolean isEmpty() {
+        return counts.isEmpty();
+    }
+
+    public boolean remove(E element) {
+        Count<E> count = find(element);
+        if (count == null) return false;
+        else return counts.remove(count);
+    }
+
+}
